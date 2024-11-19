add1be34fb47574a2521c79b8323ed8b5dd45d79
Shriram Mange <shrirammange12345@gmail.com>
2024-11-19 23:35:45 +0530

    BottomNavigationBar height set to default

--- app/src/main/java/com/shriram/fashionapp/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/shriram/fashionapp/navigation/BottomNavigationBar.kt
@@ -1,15 +1,12 @@
 package com.shriram.fashionapp.navigation
 
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -32,7 +29,6 @@ fun BottomNavigationBar(navController: NavHostController) {
     val selectedIndex = items.indexOfFirst { it.route == currentRoute }
 
     NavigationBar(
-        modifier = Modifier.height(64.dp),
         containerColor = Color.White,
         contentColor = Color.Black,
 
